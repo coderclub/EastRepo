0f61e5e6c89b2a74bb38118c33223b991133d8c2
zaya0619 <zaykamnts@gmail.com>
2018-08-28 22:56:09 -0400

    if you see something, say something

---
+++ DV.java
@@ -0,0 +1,22 @@
+package packpak;
+
+public class TestCar {
+
+	public static void main(String[]args) {
+		
+		Car AhmedCar = new Car(201, "Camry", "Toyota") ;				//Constructor 4
+		
+		Car FahimCar = new Car();										//Constructor 1
+		
+		Car FarzadCar = new Car("4 Series", "BMW", 50);					//Constructor 3
+		FarzadCar.displayCarModel("BMW");	//gvgvv
+		FarzadCar.displayCarModel("Camry");	//.hjbghvgit							//Method 1 // for ahmed
+		
+		Car ShamsCar = new Car(404, "Prius", "Toyota", 56, 'B' );		//Constructor 2
+		System.out.println(ShamsCar.GenerateSerialNo(404, 56));			//Method 2
+		ShamsCar.CarMilage();											//Method 3
+		ShamsCar.CarMake("Toyota");//Method 4
+
+		trailer hasanTrailer = new Trailer("Range Rover", "Sports")
+	}
+}
